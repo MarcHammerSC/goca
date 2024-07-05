118fa2792ef262a70b7d533c96176844314715db
MarcHammerSC <67946941+MarcHammerSC@users.noreply.github.com>
2024-07-05 13:00:47 +0200

    Update key.go

--- key/key.go
+++ key/key.go
@@ -35,7 +35,7 @@ import (
 	"crypto/x509"
 	"encoding/pem"
 
-	storage "github.com/kairoaraujo/goca/v2/_storage"
+	storage "github.com/MarcHammerSC/goca/_storage"
 )
 
 // KeysData represents the RSA keys with Private Key (Key) and Public Key (Public Key).
